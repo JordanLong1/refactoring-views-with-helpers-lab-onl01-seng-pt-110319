e5e49f0621d0c3aae0678a37d9a46cb5255bd1d6
Jordan <jordanleelong5@gmail.com>
2020-04-11 09:45:08 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,22 @@
 module ArtistsHelper
+
+   
+    # def display_artist(artist)
+    #     if artist.present? 
+    #        redirect_to artist_path(artist)
+    #     else 
+    #         redirect_to edit_artist_path(artist)
+    #     end
+    # end
+
+     
+    def display_artist(song)
+        if song.artist 
+            link_to song.artist.name, artist_path(song.artist)
+        else 
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
+
+  
 end
